052f9947717c0b6c96e23fd1c26fc2d3f92d6c2e
Jekeee <glooffee@gmail.com>
2022-11-17 16:52:09 +0200

    change-style

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const header = document.querySelector(".header");
 const scrollPosition = () =>
   window.pageYOffset || document.documentElement.crollTop;
 const containHide = () => header.classList.contains("hide");
+const upward = document.querySelector('.upward');
 
 window.addEventListener("scroll", () => {
   if (
@@ -17,5 +18,15 @@ window.addEventListener("scroll", () => {
     header.classList.remove("hide");
   }
 
+  upward.classList.toggle('active', window.scrollY > 200);
+
   lastScroll = scrollPosition();
 });
+
+function scrollToTop(){
+  window.scrollTo({
+    top:0,
+    behavior: 'smooth'
+  })
+}
+
