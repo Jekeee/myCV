c8e74f1f8dfd557d0e39d4186bca0e784dd5c4aa
Jekeee <glooffee@gmail.com>
2022-11-21 20:31:04 +0200

    chamge-style-576

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let lastScroll = 0;
 const defaultOffset = 200;
 const header = document.querySelector(".header");
-
+const burger = document.querySelector(".header-burger");
+const nav = document.querySelector(".nav");
+const body = document.querySelector("body");
 const scrollPosition = () =>
   window.pageYOffset || document.documentElement.crollTop;
 const containHide = () => header.classList.contains("hide");
-const upward = document.querySelector('.upward');
+const upward = document.querySelector(".upward");
 
 window.addEventListener("scroll", () => {
   if (
@@ -18,15 +20,32 @@ window.addEventListener("scroll", () => {
     header.classList.remove("hide");
   }
 
-  upward.classList.toggle('active', window.scrollY > 200);
+  upward.classList.toggle("active", window.scrollY > 200);
 
   lastScroll = scrollPosition();
 });
 
-function scrollToTop(){
+function scrollToTop() {
   window.scrollTo({
-    top:0,
-    behavior: 'smooth'
-  })
+    top: 0,
+    behavior: "smooth",
+  });
 }
 
+nav.addEventListener("click", () => {
+  burger.classList.remove("active");
+  nav.classList.remove("active");
+  body.classList.remove("lock");
+});
+
+burger.addEventListener("click", () => {
+  if (burger.classList.contains("active")) {
+    burger.classList.remove("active");
+    nav.classList.remove("active");
+    body.classList.remove("lock");
+  } else {
+    burger.classList.add("active");
+    nav.classList.add("active");
+    body.classList.add("lock");
+  }
+});
